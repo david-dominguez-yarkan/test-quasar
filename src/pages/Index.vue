bbc7e0e44ab9b65832592da9fe145fbfe3995dc4
ykn-davdomin <71440050+ykn-davdomin@users.noreply.github.com>
2020-09-30 15:55:11 -0500

    Adding two validator

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,36 @@
 <template>
   <q-page class="flex flex-center">
-    <q-input v-model="value" />
+    <q-form class="q-gutter-md">
+      <q-input
+        v-model="text1"
+        label="text1"
+        :rules="[
+          (val) => !!val || 'This field is required',
+          (val) => !falgs.flag1 || 'The flag1 must be activaed'
+        ]"
+      >
+      </q-input>
+      <q-input
+        v-model="text2"
+        label="text2"
+        :rules="[(val) => !!val || 'This field is required']"
+      >
+      </q-input>
+    </q-form>
   </q-page>
 </template>
+
 <script>
 export default {
   name: 'PageIndex',
   data() {
     return {
-      value: 'This is text'
+      text1: '',
+      text2: '',
+      flags: {
+        flag1: false,
+        flag2: false
+      }
     }
   }
 }
