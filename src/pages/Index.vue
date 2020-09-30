d05c7e0cf7c3bb4ee4f06751069445178d8c5897
ykn-davdomin <71440050+ykn-davdomin@users.noreply.github.com>
2020-09-30 15:23:36 -0500

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-input v-model="value" />
+  </q-page>
+</template>
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      value: 'This is text'
+    }
+  }
+}
+</script>
